096aa9799af7d8a53ddbae7e8f230d304661d214
Maksim Yersh <m.yersh@admitad.com>
2023-10-03 21:42:58 +0300

    feat: initial event create section

---
+++ pages/events/create.tsx
@@ -0,0 +1,11 @@
+import { FooterSection } from "@/components/footer";
+import { Navbar } from "@/components/navbar";
+
+export default function EventCreate() {
+  return (
+    <>
+      <Navbar />
+      <FooterSection />
+    </>
+  );
+}
