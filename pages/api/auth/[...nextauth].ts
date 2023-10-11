1ba46399f014ff7f672d77aa689589d02ba9e334
Maksim Yersh <m.yersh@admitad.com>
2023-10-11 13:31:17 +0300

    fix: integrate email auth with scw transactional email

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -14,6 +14,10 @@ export const authOptions: NextAuthOptions = {
       server: {
         host: process.env.EMAIL_SERVER_HOST,
         port: process.env.EMAIL_SERVER_PORT,
+        auth: {
+          user: process.env.EMAIL_SERVER_USER,
+          pass: process.env.EMAIL_SERVER_PASSWORD,
+        },
       },
       from: process.env.EMAIL_FROM,
     }),
