3cc3753efb6599ecde0a0c13d9c56fda28564d92
kiruushaaa <kiruxa4866613@gmail.com>
2024-01-24 17:51:53 +0300

    fix: removed nonexistent prop

--- src/app/[locale]/uikit/page.tsx
+++ src/app/[locale]/uikit/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/uikit";
 export default function Uikit() {
   return (
     <>
-      <Button status={"success"}>Hello</Button>
+      <Button>Hello</Button>
     </>
   );
 }
