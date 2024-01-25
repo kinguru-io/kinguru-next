2b79eaf5e72e8c50d9db3fd972e34941aaa6e044
Kirill <kiruxa4866613@gmail.com>
2024-01-25 10:31:02 +0300

    feat(KINGU-50): primary UIkit button structure with stories (#345)
    * feat: added base for uikit button and corresponding story
    
    * fix: removed nonexistent prop
    
    * fix: add pre-compile task for prisma client generation
    
    * feat: added icon functionality to Button with corresponding stories
    
    * chore: disabled eslint rule for storybook
    
    ---------
    
    Co-authored-by: Maksim Yersh <edelwud@icloud.com>

--- src/components/uikit/Button/Button.tsx
+++ src/components/uikit/Button/Button.tsx
@@ -1,23 +1,72 @@
-import { cva } from "~/styled-system/css";
-import { styled } from "~/styled-system/jsx";
+import { ComponentProps } from "react";
+import { cva, type RecipeVariantProps } from "~/styled-system/css";
 
 export const button = cva({
   base: {
-    fontWeight: "medium",
-    borderRadius: "md",
+    display: "flex",
+    alignItems: "center",
+    gap: "1",
+    borderRadius: "3xl",
+    color: "slate.950",
+    border: "0.5px solid",
+    cursor: "pointer",
   },
   variants: {
-    status: {
-      default: {
-        color: "white",
-        bg: "gray.500",
+    variant: {
+      primary: {
+        bg: "yellow.300",
+        borderColor: "yellow.300",
+        fontWeight: "bold",
+        _hover: {
+          bg: "yellow.200",
+          borderColor: "yellow.200",
+        },
       },
-      success: {
-        color: "white",
-        bg: "green.500",
+      outline: {
+        bg: "slate.50",
+        borderColor: "slate.950",
+        fontWeight: "normal",
+        _hover: {
+          textDecoration: "underline",
+        },
+      },
+    },
+    size: {
+      sm: {
+        fontSize: "md",
+        lineHeight: "normal",
+        px: "25px",
+        py: "8px",
+      },
+    },
+    iconPosition: {
+      left: {
+        flexDirection: "row",
+      },
+      right: {
+        flexDirection: "row-reverse",
       },
     },
   },
 });
 
-export const Button = styled("button", button);
+type ButtonProps = {
+  icon?: React.ReactNode;
+} & RecipeVariantProps<typeof button> &
+  ComponentProps<"button">;
+
+export function Button({
+  icon = null,
+  iconPosition = "left",
+  variant = "primary",
+  size = "sm",
+  children,
+  ...restProps
+}: ButtonProps) {
+  return (
+    <button className={button({ variant, size, iconPosition })} {...restProps}>
+      {icon && <span>{icon}</span>}
+      {children}
+    </button>
+  );
+}
