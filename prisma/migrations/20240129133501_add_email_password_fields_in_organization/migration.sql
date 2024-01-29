43ab56cc872a9fb2ef224a9af3947b88fc217cca
alexander <okyxxxf@gmail.com>
2024-01-29 16:36:17 +0300

    fix: made email field unique

--- prisma/migrations/20240129133501_add_email_password_fields_in_organization/migration.sql
+++ prisma/migrations/20240129133501_add_email_password_fields_in_organization/migration.sql
@@ -1,6 +1,7 @@
 /*
   Warnings:
 
+  - A unique constraint covering the columns `[email]` on the table `Organization` will be added. If there are existing duplicate values, this will fail.
   - Added the required column `email` to the `Organization` table without a default value. This is not possible if the table is not empty.
   - Added the required column `password` to the `Organization` table without a default value. This is not possible if the table is not empty.
 
@@ -9,3 +10,6 @@
 ALTER TABLE "Organization" ADD COLUMN     "email" TEXT NOT NULL,
 ADD COLUMN     "emailVerified" TIMESTAMP(3),
 ADD COLUMN     "password" TEXT NOT NULL;
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Organization_email_key" ON "Organization"("email");
