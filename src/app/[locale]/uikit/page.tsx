fa1b71a64743c6c2f6c341a8981076a80dfcc6e6
alexander <okyxxxf@gmail.com>
2024-02-08 09:38:46 +0300

    fix: remove buttons from [locale]/uikit

--- src/app/[locale]/uikit/page.tsx
+++ src/app/[locale]/uikit/page.tsx
@@ -2,27 +2,8 @@ import { Button } from "@/components/uikit";
 
 export default function Uikit() {
   return (
-    <div style={{ display: "flex", gap: "10px" }}>
-      <div style={{ display: "flex", gap: "10px", alignItems: "center" }}>
-        <Button size={"sm"}>Hello</Button>
-        <Button size={"md"}>Hello</Button>
-        <Button size={"lg"}>Hello</Button>
-        <Button size={"xl"}>Hello</Button>
-      </div>
-      <div style={{ display: "flex", gap: "10px", alignItems: "center" }}>
-        <Button variant={"outline"} size={"sm"}>
-          Hello
-        </Button>
-        <Button variant={"outline"} size={"md"}>
-          Hello
-        </Button>
-        <Button variant={"outline"} size={"lg"}>
-          Hello
-        </Button>
-        <Button variant={"outline"} size={"xl"}>
-          Hello
-        </Button>
-      </div>
-    </div>
+    <>
+      <Button>Hello</Button>
+    </>
   );
 }
