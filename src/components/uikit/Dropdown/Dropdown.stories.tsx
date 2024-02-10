3a920e0781ef704dd59bd8a6abee46d779400e7b
alexander <okyxxxf@gmail.com>
2024-02-10 16:53:03 +0300

    feat: add dropdown to storybook

---
+++ src/components/uikit/Dropdown/Dropdown.stories.tsx
@@ -0,0 +1,41 @@
+/* eslint-disable import/no-extraneous-dependencies */
+
+import { Meta, StoryObj } from "@storybook/react";
+import { Dropdown } from "./Dropdown";
+import { dropdown } from "~/styled-system/recipes";
+
+const meta = {
+  title: "UIKit/Dropdown/Dropdown",
+  component: Dropdown,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+  argTypes: {
+    size: {
+      options: dropdown.variantMap.size,
+      control: "radio",
+    },
+  },
+} satisfies Meta<typeof Dropdown>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const DefaultDropdown: Story = {
+  args: {
+    size: "sm",
+    children: [<div>Sd</div>, <div>Sd</div>],
+  },
+};
+
+export const SizedDropdown: Story = {
+  args: DefaultDropdown.args,
+  render: ({ children }) => {
+    const data = dropdown.variantMap.size.map((value) => {
+      return <Dropdown size={value}>{children}</Dropdown>;
+    });
+
+    return <>{data}</>;
+  },
+};
