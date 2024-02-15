26a4ac5dc2893f9006c97ad191fb387093d606b9
alexander <okyxxxf@gmail.com>
2024-02-15 22:01:15 +0300

    fix: remove header from uikit page

--- src/app/[locale]/uikit/page.tsx
+++ src/app/[locale]/uikit/page.tsx
@@ -1,10 +1,8 @@
-import { Header } from "../../../components/header/header";
 import { Button } from "@/components/uikit";
 
 export default function Uikit() {
   return (
     <>
-      <Header />
       <Button>Hello</Button>
     </>
   );
