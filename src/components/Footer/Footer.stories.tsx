a0824771c60db0bec265fbf15e0bf6fd05ba3040
alexander <okyxxxf@gmail.com>
2024-02-17 23:12:43 +0300

    feat: create story for footer

---
+++ src/components/Footer/Footer.stories.tsx
@@ -0,0 +1,59 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { StoryObj } from "@storybook/react";
+import { NextIntlClientProvider } from "next-intl";
+import { Footer } from "./Footer";
+import englishLocale from "../../../public/locales/en/common.json";
+import polishLocale from "../../../public/locales/pl/common.json";
+import russianLocale from "../../../public/locales/ru/common.json";
+
+const meta = {
+  title: "Footer",
+  component: () => (
+    <NextIntlClientProvider locale="ru" messages={russianLocale}>
+      <Footer />
+    </NextIntlClientProvider>
+  ),
+  tags: ["autodocs"],
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const russianFooter: Story = {
+  parameters: {
+    nextjs: {
+      appDirectory: true,
+    },
+  },
+  render: () => (
+    <NextIntlClientProvider locale="ru" messages={russianLocale}>
+      <Footer />
+    </NextIntlClientProvider>
+  ),
+};
+
+export const englishFooter: Story = {
+  parameters: {
+    nextjs: {
+      appDirectory: true,
+    },
+  },
+  render: () => (
+    <NextIntlClientProvider locale="en" messages={englishLocale}>
+      <Footer />
+    </NextIntlClientProvider>
+  ),
+};
+
+export const polishFooter: Story = {
+  parameters: {
+    nextjs: {
+      appDirectory: true,
+    },
+  },
+  render: () => (
+    <NextIntlClientProvider locale="pl" messages={polishLocale}>
+      <Footer />
+    </NextIntlClientProvider>
+  ),
+};
