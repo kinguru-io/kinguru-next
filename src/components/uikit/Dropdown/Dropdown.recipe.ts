8dd29ff31797026ab936c8d783533a84cdf697a6
alexander <okyxxxf@gmail.com>
2024-02-22 18:06:43 +0300

    feat: change color to token (dropdown)

--- src/components/uikit/Dropdown/Dropdown.recipe.ts
+++ src/components/uikit/Dropdown/Dropdown.recipe.ts
@@ -17,7 +17,7 @@ export const dropdownSlot = defineSlotRecipe({
       bg: "token(colors.neutral.5)",
       right: "0",
       minW: "110px",
-      color: "#000000",
+      color: "token(colors.neutral.1)",
       zIndex: "token(zIndex.dropdown)",
       "&[data-hidden=true]": { display: "none" },
     },
