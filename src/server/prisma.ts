599a80936e5488db1a01fc0741ee87702b6101aa
kiruushaaa <kiruxa4866613@gmail.com>
2024-03-04 19:29:09 +0300

    fix: having single prisma instance

--- src/server/prisma.ts
+++ src/server/prisma.ts
@@ -1,11 +1,5 @@
 import { PrismaClient } from "@prisma/client";
 
-let prisma: PrismaClient;
-
-if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient();
-} else {
-  prisma = new PrismaClient();
-}
+const prisma = (globalThis.prisma ??= new PrismaClient());
 
 export default prisma;
