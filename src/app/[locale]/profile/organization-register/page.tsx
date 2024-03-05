f9683333b27818e6842d63ec9d983cdb4c465de8
Maksim Yersh <edelwud@icloud.com>
2024-03-05 22:24:26 +0300

    feat: add basic form for organization registration

--- src/app/[locale]/profile/organization-register/page.tsx
+++ src/app/[locale]/profile/organization-register/page.tsx
@@ -1,14 +1,15 @@
 import { useTranslations } from "next-intl";
-import { BasicInfoForm } from "./form";
+import { OrganizationRegisterForm } from "./form";
+import { orgRegister } from "@/lib/actions";
 
-export default function ProfileBasicInfoPage() {
+export default function OrganizationRegisterPage() {
   const t = useTranslations("organization.basic_info_page");
 
   return (
     <>
       <h1 className="heading">{t("heading")}</h1>
       <section>
-        <BasicInfoForm />
+        <OrganizationRegisterForm orgRegister={orgRegister} />
       </section>
     </>
   );
