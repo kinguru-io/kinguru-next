ad688e555256ed9929af5d490692c9014b03a7ac
kiruushaaa <kiruxa4866613@gmail.com>
2024-03-19 19:16:01 +0300

    feat: added temporary scroll snap for responsive view; adjusted grid layout to be correct without month select

--- src/components/calendar/WeekView/WeekView.tsx
+++ src/components/calendar/WeekView/WeekView.tsx
@@ -9,7 +9,7 @@ import { Button, TimeSlot, type TimeSlotInfo } from "@/components/uikit";
 import { getWeekViewData, DAYS_OF_WEEK_ORDERED } from "@/lib/utils/datetime";
 import type { Locale } from "@/navigation";
 import { css } from "~/styled-system/css";
-import { Box, Grid, GridItem, VStack } from "~/styled-system/jsx";
+import { Box, Flex, Grid, GridItem, VStack } from "~/styled-system/jsx";
 
 export function WeekView({
   locale,
@@ -39,109 +39,120 @@ export function WeekView({
   const weekViewData = getWeekViewData({ locale, originDate });
 
   return (
-    <Box>
-      <MonthSelect
-        locale={locale}
-        monthNumber={currentMonthNumber}
-        changeMonth={changeMonth}
-        initialDate={todayDate}
-        endDate={lastAllowedDate}
-      />
-      <Grid
-        gridTemplateColumns="auto 1fr auto"
-        // TODO adjust layout to use common Grid parent for all the controls
-        gridTemplateAreas="'prev-week . next-week'"
+    <Grid
+      gap="0"
+      gridTemplateColumns="auto 1fr auto"
+      gridTemplateRows="repeat(3, auto)"
+      gridTemplateAreas="'prev-week month-select next-week' 'prev-week week-view next-week'"
+    >
+      <GridItem gridArea="month-select">
+        <MonthSelect
+          locale={locale}
+          monthNumber={currentMonthNumber}
+          changeMonth={changeMonth}
+          initialDate={todayDate}
+          endDate={lastAllowedDate}
+        />
+      </GridItem>
+      <GridItem gridArea="prev-week" gridRow="2 / 2">
+        <Button
+          type="button"
+          variant="ghost"
+          onClick={prevWeek}
+          disabled={canGoPrev}
+        >
+          <GrNext size={30} className={css({ rotate: "180deg" })} />
+        </Button>
+      </GridItem>
+      <GridItem gridArea="next-week" gridRow="2 / 2">
+        <Button
+          type="button"
+          variant="ghost"
+          onClick={nextWeek}
+          disabled={canGoNext}
+        >
+          <GrNext size={30} />
+        </Button>
+      </GridItem>
+
+      <Flex
+        gridArea="week-view"
+        gap="10px"
+        overflowX="auto"
+        scrollSnapType="x mandatory"
       >
-        <GridItem gridArea="prev-week">
-          <Button
-            type="button"
-            variant="ghost"
-            onClick={prevWeek}
-            disabled={canGoPrev}
-          >
-            <GrNext size={30} className={css({ rotate: "180deg" })} />
-          </Button>
-        </GridItem>
+        {weekViewData.map((weekdayInfo, idx) => {
+          const dayOfWeekGroupKey = DAYS_OF_WEEK_ORDERED[idx];
 
-        <GridItem gridArea="next-week">
-          <Button
-            type="button"
-            variant="ghost"
-            onClick={nextWeek}
-            disabled={canGoNext}
-          >
-            <GrNext size={30} />
-          </Button>
-        </GridItem>
-
-        <Grid gridTemplateColumns="7" gap="10px">
-          {weekViewData.map((weekdayInfo, idx) => {
-            const dayOfWeekGroupKey = DAYS_OF_WEEK_ORDERED[idx];
-
-            return (
-              <VStack gap="15px" key={weekdayInfo.weekdayShort}>
-                <Box
-                  alignSelf="stretch"
-                  borderBlockEnd="4px solid token(colors.primary.disabled)"
-                  paddingBlockEnd="3px"
-                  marginBlockEnd="5px"
-                  aria-selected={weekdayInfo.isToday}
-                  css={{
-                    textAlign: "center",
-                    _selected: {
-                      borderColor: "primary",
-                    },
-                  }}
+          return (
+            <VStack
+              key={weekdayInfo.weekdayShort}
+              gap="15px"
+              scrollSnapAlign="start"
+              minWidth="57px"
+              flexBasis="full"
+            >
+              <Box
+                alignSelf="stretch"
+                borderBlockEnd="4px solid token(colors.primary.disabled)"
+                paddingBlockEnd="3px"
+                marginBlockEnd="5px"
+                aria-selected={weekdayInfo.isToday}
+                css={{
+                  textAlign: "center",
+                  _selected: {
+                    borderColor: "primary",
+                  },
+                }}
+              >
+                <time
+                  className={css({
+                    textStyle: "body.1",
+                    textTransform: "capitalize",
+                    whiteSpace: "pre",
+                  })}
+                  dateTime={lightFormat(weekdayInfo.day, "yyyy-MM-dd")}
+                  suppressHydrationWarning
                 >
-                  <time
-                    className={css({
-                      textStyle: "body.1",
-                      textTransform: "capitalize",
-                      whiteSpace: "pre",
-                    })}
-                    dateTime={lightFormat(weekdayInfo.day, "yyyy-MM-dd")}
-                    suppressHydrationWarning
-                  >
-                    {weekdayInfo.weekdayShort}
-                    {"\n"}
-                    {lightFormat(weekdayInfo.day, "d")}
-                  </time>
-                </Box>
+                  {weekdayInfo.weekdayShort}
+                  {"\n"}
+                  {lightFormat(weekdayInfo.day, "d")}
+                </time>
+              </Box>
 
-                {timeSlotsGroup[dayOfWeekGroupKey].map(({ timeSlots }) => {
-                  return timeSlots.flatMap(({ price, time }) => {
-                    const slotTime = set(weekdayInfo.day, {
-                      hours: time.getHours(),
-                      minutes: time.getMinutes(),
-                      seconds: 0,
-                      milliseconds: 0,
-                    });
-
-                    if (isPast(slotTime)) return null;
-
-                    const slotISOString = slotTime.toISOString();
-
-                    return (
-                      <TimeSlot
-                        key={slotISOString}
-                        price={price}
-                        time={slotTime}
-                        onClick={() => {
-                          console.log(slotISOString);
-                        }}
-                        condition={
-                          // ! DEBUG-only condition
-                          bookedSlots.has(slotISOString) ? "max" : "regular"
-                        }
-                      />
-                    );
+              {timeSlotsGroup[dayOfWeekGroupKey].map(({ timeSlots }) => {
+                return timeSlots.flatMap(({ price, time }) => {
+                  const slotTime = set(weekdayInfo.day, {
+                    hours: time.getHours(),
+                    minutes: time.getMinutes(),
+                    seconds: 0,
+                    milliseconds: 0,
                   });
-                })}
-              </VStack>
-            );
-          })}
-        </Grid>
-      </Grid>
-    </Box>
+
+                  if (isPast(slotTime)) return null;
+
+                  const slotISOString = slotTime.toISOString();
+
+                  return (
+                    <TimeSlot
+                      key={slotISOString}
+                      price={price}
+                      time={slotTime}
+                      onClick={() => {
+                        console.log(slotISOString);
+                      }}
+                      condition={
+                        // ! DEBUG-only condition
+                        bookedSlots.has(slotISOString) ? "max" : "regular"
+                      }
+                    />
+                  );
+                });
+              })}
+            </VStack>
+          );
+        })}
+      </Flex>
+    </Grid>
   );
 }
