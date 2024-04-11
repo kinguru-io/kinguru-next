42a7be4158295fdbe18ee347dd9c6f9c7e697bf7
Maksim Yersh <51487247+edelwud@users.noreply.github.com>
2024-04-11 06:31:28 +0000

    fix: update stripe api version

--- src/lib/actions/event/order/ticketIntent.ts
+++ src/lib/actions/event/order/ticketIntent.ts
@@ -7,7 +7,7 @@ import { redirect } from "@/navigation.ts";
 import prisma from "@/server/prisma.ts";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2023-10-16",
+  apiVersion: "2024-04-10",
 });
 
 export async function getTicketIntentAction(eventId: string) {
