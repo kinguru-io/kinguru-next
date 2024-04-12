b3638a7204ec8bf19359eb342a0dfef646d93402
kiruushaaa <kiruxa4866613@gmail.com>
2024-04-12 16:24:27 +0300

    chore: adjusted `AvatarProps`

--- src/components/uikit/Avatar/Avatar.tsx
+++ src/components/uikit/Avatar/Avatar.tsx
@@ -1,12 +1,9 @@
 import Image from "next/image";
-import React from "react";
+import type { User } from "next-auth";
 import defaultAvatar from "~/public/img/defaultImages/defaultUserAvatar.svg";
 import { avatar, type AvatarVariantProps } from "~/styled-system/recipes";
 
-export type AvatarDataType = {
-  image: string | null;
-  name: string | null;
-};
+export type AvatarDataType = Pick<User, "name" | "image">;
 
 type AvatarProps = AvatarVariantProps & AvatarDataType;
 
