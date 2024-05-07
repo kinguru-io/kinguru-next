526de5526f73f22ccd58699223388c55e2656e35
kiruushaaa <kiruxa4866613@gmail.com>
2024-05-07 18:19:34 +0300

    feat: initial notifications page

--- src/app/[locale]/profile/notifications/page.tsx
+++ src/app/[locale]/profile/notifications/page.tsx
@@ -1,7 +1,25 @@
-export default function NotificationsPage() {
+import { getTranslations } from "next-intl/server";
+import { getSession } from "@/auth";
+import { ProfileNotifications } from "@/components/notifications/profile-notifications";
+import { ProfileSectionLayout } from "@/layout/page";
+import { Stack } from "~/styled-system/jsx";
+
+export default async function NotificationsPage() {
+  const t = await getTranslations("profile.notifications");
+  const session = await getSession();
+  const notifications = await prisma.purchaseNotification.findMany({
+    where: { userId: session?.user?.id },
+    orderBy: { createdAt: "desc" },
+  });
+
   return (
-    <div>
-      <h1>NotificationsPage</h1>
-    </div>
+    <ProfileSectionLayout>
+      <h1 className="heading">{t("heading")}</h1>
+      <section>
+        <Stack gap="10px">
+          <ProfileNotifications notifications={notifications} />
+        </Stack>
+      </section>
+    </ProfileSectionLayout>
   );
 }
