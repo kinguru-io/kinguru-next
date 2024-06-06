8d9b1fb6f88bd06b410f028b0f37f546517aa73b
kiruushaaa <kiruxa4866613@gmail.com>
2024-06-06 15:29:15 +0300

    feat: cleaned up consumer; added signals handling; added `PremiseSlot` indexation; changed the way open hours are indexed

--- consumer/main.ts
+++ consumer/main.ts
@@ -1,22 +1,23 @@
 import { Client } from "@elastic/elasticsearch";
-import { ResponseError } from "@elastic/transport/lib/errors";
+import { ResponseError } from "@elastic/elasticsearch/lib/errors";
 // ! do not forget to uncomment when using the mapbox service
 // import mapbox, { type SearchBoxSuggestion } from "@mapbox/search-js-core";
 import { PrismaClient } from "@prisma/client";
+
 import * as dotenv from "dotenv";
 import { Kafka } from "kafkajs";
-import { groupBy } from "../src/lib/shared/utils/array";
-import { DAYS_OF_WEEK_ORDERED } from "../src/lib/shared/utils/datetime";
+import { upsertPremiseFulfilledIndex } from "./processors/premise-fulfilled";
 
 dotenv.config();
 
 const esApiKey = process.env.ES_CLIENT_API_KEY;
-// ! do not forget to uncomment when using the mapbox service
-// const mapboxAccessToken = process.env.NEXT_PUBLIC_MAPBOX_TOKEN;
 const premiseFulfilledIndex =
   process.env.ES_INDEX_PREMISE_FULFILLED || "kinguru.public.premise_fulfilled";
 
-const premiseTopicRegex = /^.*\.public\.Premise$/m;
+const premiseTopic =
+  process.env.KAFKA_TOPIC_PREMISE || "kinguru.public.Premise";
+const premiseSlotTopic =
+  process.env.KAFKA_TOPIC_PREMISESLOT || "kinguru.public.PremiseSlot";
 
 const prisma = new PrismaClient();
 const kafka = new Kafka({
@@ -24,23 +25,22 @@ const kafka = new Kafka({
   brokers: [process.env.KAFKA_BROKER || ""],
   connectionTimeout: 3000,
 });
+const consumer = kafka.consumer({ groupId: "premise_fulfill" });
+const logger = consumer.logger();
 const esClient = new Client({
   node: process.env.ES_CLIENT_NODE,
   ...(esApiKey && { auth: { apiKey: esApiKey } }),
 });
 // ! do not forget to uncomment when using the mapbox service
 // const searchBox = new mapbox.SearchBoxCore({
-//   accessToken: mapboxAccessToken,
+//   accessToken: process.env.NEXT_PUBLIC_MAPBOX_TOKEN,
 // });
 
 async function init() {
   await prisma.$connect();
-
-  const consumer = kafka.consumer({ groupId: "premise_fulfill" });
-
   await consumer.connect();
   await consumer.subscribe({
-    topic: premiseTopicRegex,
+    topics: [premiseTopic, premiseSlotTopic],
     fromBeginning: true,
   });
 
@@ -51,9 +51,25 @@ async function init() {
   } catch (e) {
     if (e instanceof ResponseError) {
       if (e.body?.error.type === "index_not_found_exception") {
-        await esClient.indices.create({
+        const { statusCode } = await esClient.indices.create({
           index: premiseFulfilledIndex,
         });
+
+        if (statusCode && [200, 201].includes(statusCode)) {
+          await esClient.indices.putMapping({
+            index: premiseFulfilledIndex,
+            body: {
+              properties: {
+                booked_slots: {
+                  type: "join",
+                  relations: {
+                    premise: "premise_slot",
+                  },
+                },
+              },
+            },
+          });
+        }
       }
     }
   }
@@ -61,119 +77,110 @@ async function init() {
   await consumer.run({
     eachMessage: async ({ topic, partition, message }) => {
       if (message.key === null) {
-        console.log(`[${topic}]: PART:${partition}:`, "empty key");
+        logger.warn("empty key");
         return;
       }
 
       const messageKey = message.key.toString();
-      console.log(`[${topic}]: PART:${partition}:`, messageKey);
-
-      // only care about Premise records for now
-      if (!premiseTopicRegex.test(topic)) {
-        return;
-      }
+      logger.info(messageKey);
 
       const {
-        payload: { id: premiseId },
+        payload: { id },
       } = JSON.parse(messageKey) as { payload: { id: string } };
 
       // `value === null` means Premise record was deleted
       if (message.value === null) {
-        await esClient.delete({ index: premiseFulfilledIndex, id: premiseId });
+        try {
+          await esClient.delete({
+            index: premiseFulfilledIndex,
+            id,
+          });
+        } catch (e) {
+          if (e instanceof ResponseError) {
+            logger.error(e.message);
+          }
+        }
         return;
       }
 
-      const premise = await prisma.premise.findUnique({
-        where: { id: premiseId },
-        include: {
-          venue: {
-            select: { name: true, description: true, locationMapboxId: true },
-          },
-          openHours: {
-            orderBy: { price: "asc" },
-            select: { day: true, openTime: true, closeTime: true, price: true },
-          },
-        },
-      });
-
-      if (!premise) {
-        console.log(
-          `[${topic}]: PART:${partition}:`,
-          `no Premise record exists with id: ${premiseId}`,
-        );
-        return;
+      if (topic === premiseTopic) {
+        await upsertPremiseFulfilledIndex({
+          id,
+          index: premiseFulfilledIndex,
+          prisma,
+          esClient,
+          logger,
+        });
       }
 
-      const { venue, openHours, ...restPremise } = premise;
+      if (topic === premiseSlotTopic) {
+        const premiseSlot = await prisma.premiseSlot.findUnique({
+          where: { id },
+          select: {
+            id: true,
+            premiseId: true,
+            userId: true,
+            date: true,
+            startTime: true,
+            endTime: true,
+            status: true,
+          },
+        });
 
-      // mapping to the form `{ open: number; close: number }` instead of date string
-      const mappedOpenHours = openHours.map(({ day, openTime, closeTime }) => ({
-        day,
-        open: openTime.getUTCHours(),
-        close: closeTime.getUTCHours(),
-      }));
+        if (!premiseSlot) {
+          logger.error(`no PremiseSlot record exists with id: ${id}`);
+          return;
+        }
 
-      // grouping like that in order to get rid of ES nested fields
-      const groupedOpenHours = groupBy(
-        mappedOpenHours,
-        ({ day }) => DAYS_OF_WEEK_ORDERED.indexOf(day) + 1,
-      );
+        const { premiseId, ...restSlot } = premiseSlot;
 
-      const location = await prepareIndexLocation(venue.locationMapboxId);
-
-      await esClient.index({
-        index: premiseFulfilledIndex,
-        id: restPremise.id,
-        body: {
-          ...restPremise,
-          ...location,
-          minPrice: openHours.at(0)?.price,
-          maxPrice: openHours.at(-1)?.price,
-          openHours: groupedOpenHours,
-          "venue.name": venue.name,
-          "venue.description": venue.description,
-        },
-      });
+        await esClient.index({
+          index: premiseFulfilledIndex,
+          id,
+          routing: premiseId,
+          body: {
+            ...restSlot,
+            booked_slots: {
+              name: "premise_slot",
+              parent: premiseId,
+            },
+          },
+        });
+      }
     },
   });
 }
 
 void init();
 
-async function prepareIndexLocation(_mapboxId: string) {
-  return {
-    "location.countryCode": "PL",
-    "location.city": "Warsaw",
-    "location.timeZone": "Europe/Warsaw",
-    "location.latitude": 52.237049,
-    "location.longitude": 21.017532,
-  };
+const errorTypes = ["unhandledRejection", "uncaughtException"];
+const signalTraps = ["SIGTERM", "SIGINT", "SIGUSR2"];
 
-  // ! do not forget to uncomment when using the mapbox service
-  //   const mapboxResponse = await searchBox.retrieve(
-  //     {
-  //       mapbox_id: mapboxId,
-  //     } as SearchBoxSuggestion,
-  //     {
-  //       sessionToken: "test-123",
-  //     },
-  //   );
-  //   const locationProperties = mapboxResponse.features.at(0)?.properties;
-  //   const locationContext = locationProperties?.context || {};
-  //   const longitude = locationProperties?.coordinates.longitude;
-  //   const latitude = locationProperties?.coordinates.latitude;
+errorTypes.forEach((type) => {
+  process.on(type, async (e) => {
+    try {
+      console.log(`process.on ${type}`);
+      console.error(e);
 
-  //   const tileQueryInstance = await fetch(
-  //     `https://api.mapbox.com/v4/examples.4ze9z6tv/tilequery/${longitude},${latitude}.json?access_token=${mapboxAccessToken}`,
-  //   );
-  //   const responseJson: { features: Array<{ properties: { TZID: string } }> } =
-  //     await tileQueryInstance.json();
+      await consumer.stop();
+      await Promise.allSettled([consumer.disconnect, prisma.$disconnect]);
 
-  //   return {
-  //     "location.countryCode": locationContext.country?.country_code,
-  //     "location.city": locationContext.place?.name,
-  //     "location.timeZone": responseJson.features[0].properties.TZID,
-  //     "location.longitude": longitude,
-  //     "location.latitude": latitude,
-  //   };
-}
+      process.exit(0);
+    } catch (_) {
+      process.exit(1);
+    }
+  });
+});
+
+signalTraps.forEach((type) => {
+  process.once(type, async () => {
+    console.log("\nExiting...");
+    try {
+      await consumer.stop();
+      await Promise.allSettled([consumer.disconnect(), prisma.$disconnect()]);
+    } finally {
+      console.log("\nExited");
+      process.kill(process.pid, type);
+    }
+  });
+});
