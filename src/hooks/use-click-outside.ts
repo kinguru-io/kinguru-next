e18dac9d67bea87ce611314eaf1d1000c05b73c5
kiruushaaa <kiruxa4866613@gmail.com>
2024-06-06 15:42:59 +0300

    feat: added click outside handler

---
+++ src/hooks/use-click-outside.ts
@@ -0,0 +1,27 @@
+import { type RefObject, useCallback, useEffect } from "react";
+
+/**
+ * @link source: https://github.com/Keized/react-hooks-hub/blob/main/packages/use-click-outside/src/index.ts
+ */
+export function useClickOutside(
+  refs: Array<RefObject<HTMLElement>>,
+  callback: (isOutside: boolean) => unknown,
+): void {
+  const handleClickOutside = useCallback(
+    (event: MouseEvent) => {
+      const isOutside = refs.every((ref) => {
+        return ref.current && !ref.current.contains(event.target as Node);
+      });
+
+      callback(isOutside);
+    },
+    [refs, callback],
+  );
+
+  useEffect(() => {
+    document.addEventListener("click", handleClickOutside);
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, [handleClickOutside]);
+}
