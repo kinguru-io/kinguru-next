3f1c4db84b76fbbad7f813a21e712c531038a144
kiruushaaa <kiruxa4866613@gmail.com>
2024-06-13 18:45:44 +0300

    chore: removed debounce from group filters

--- src/components/filters/filter-group-wrapper.tsx
+++ src/components/filters/filter-group-wrapper.tsx
@@ -2,7 +2,6 @@
 
 import { useSearchParams } from "next/navigation";
 import { useEffect, useRef } from "react";
-import { useDebouncedCallback } from "use-debounce";
 import { usePathname, useRouter } from "@/navigation";
 import { Stack } from "~/styled-system/jsx";
 
@@ -28,35 +27,36 @@ export function FilterGroupWrapper({
     inputs.forEach((input) => {
       input.checked = searchParams.has(input.name, input.value);
     });
-  }, [wrapperRef, searchParams]);
+  }, [wrapperRef]);
 
-  const updateGroupedSearchParams = useDebouncedCallback(
-    (name: string, value: string, checked: boolean) => {
-      if (!searchParams) return;
-      const params = new URLSearchParams(searchParams);
+  const updateGroupedSearchParams = (
+    name: string,
+    value: string,
+    checked: boolean,
+  ) => {
+    if (!searchParams) return;
+    const params = new URLSearchParams(searchParams);
 
-      switch (true) {
-        case shouldReplace: {
-          params.set(name, value);
-          break;
-        }
-        case params.has(name, value) && !checked: {
-          params.delete(name, value);
-          break;
-        }
-        case !params.has(name, value) && checked: {
-          params.append(name, value);
-          break;
-        }
-        default:
+    switch (true) {
+      case shouldReplace: {
+        params.set(name, value);
+        break;
       }
+      case params.has(name, value) && !checked: {
+        params.delete(name, value);
+        break;
+      }
+      case !params.has(name, value) && checked: {
+        params.append(name, value);
+        break;
+      }
+      default:
+    }
 
-      // resetting size in case any of filter aggs was changed
-      params.delete("size");
-      router.push(`${pathname}?${params}`, { scroll: false });
-    },
-    300,
-  );
+    // resetting size in case any of filter aggs was changed
+    params.delete("size");
+    router.push(`${pathname}?${params}`, { scroll: false });
+  };
 
   const innerInputChanged = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value, checked } = e.target;
