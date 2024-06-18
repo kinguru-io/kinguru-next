628dd44d17e6642297e4a93d84feb0816a05897c
Kirill <kiruxa4866613@gmail.com>
2024-06-18 21:54:18 +0300

    chore: removed debounce from group filters (#617)
    Fixes #

--- src/components/filters/filter-group-wrapper.tsx
+++ src/components/filters/filter-group-wrapper.tsx
@@ -1,10 +1,9 @@
 "use client";
 
 import { useSearchParams } from "next/navigation";
-import { useEffect, useRef } from "react";
-import { useDebouncedCallback } from "use-debounce";
+import { useEffect, useRef, useTransition } from "react";
 import { usePathname, useRouter } from "@/navigation";
-import { Stack } from "~/styled-system/jsx";
+import { css } from "~/styled-system/css";
 
 export function FilterGroupWrapper({
   shouldReplace = false,
@@ -16,7 +15,8 @@ export function FilterGroupWrapper({
   const pathname = usePathname();
   const router = useRouter();
   const searchParams = useSearchParams();
-  const wrapperRef = useRef<HTMLDivElement | null>(null);
+  const wrapperRef = useRef<HTMLFieldSetElement | null>(null);
+  const [pending, startTransition] = useTransition();
 
   useEffect(() => {
     if (!searchParams) return;
@@ -28,44 +28,58 @@ export function FilterGroupWrapper({
     inputs.forEach((input) => {
       input.checked = searchParams.has(input.name, input.value);
     });
-  }, [wrapperRef, searchParams]);
+  }, [wrapperRef]);
 
-  const updateGroupedSearchParams = useDebouncedCallback(
-    (name: string, value: string, checked: boolean) => {
-      if (!searchParams) return;
-      const params = new URLSearchParams(searchParams);
+  const updateGroupedSearchParams = (
+    name: string,
+    value: string,
+    checked: boolean,
+  ) => {
+    if (!searchParams) return;
+    const params = new URLSearchParams(searchParams);
 
-      switch (true) {
-        case shouldReplace: {
-          params.set(name, value);
-          break;
-        }
-        case params.has(name, value) && !checked: {
-          params.delete(name, value);
-          break;
-        }
-        case !params.has(name, value) && checked: {
-          params.append(name, value);
-          break;
-        }
-        default:
+    switch (true) {
+      case shouldReplace: {
+        params.set(name, value);
+        break;
       }
+      case params.has(name, value) && !checked: {
+        params.delete(name, value);
+        break;
+      }
+      case !params.has(name, value) && checked: {
+        params.append(name, value);
+        break;
+      }
+      default:
+    }
 
-      // resetting size in case any of filter aggs was changed
-      params.delete("size");
-      router.push(`${pathname}?${params}`, { scroll: false });
-    },
-    300,
-  );
+    // resetting size in case any of filter aggs was changed
+    params.delete("size");
+    router.push(`${pathname}?${params}`, { scroll: false });
+  };
 
-  const innerInputChanged = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value, checked } = e.target;
-    updateGroupedSearchParams(name, value, checked);
+  const innerInputChanged = ({
+    target,
+  }: React.FormEvent<HTMLFieldSetElement>) => {
+    if (isInputTarget(target)) {
+      const { name, value, checked } = target;
+      startTransition(() => updateGroupedSearchParams(name, value, checked));
+    }
   };
 
   return (
-    <Stack ref={wrapperRef} gap="8px" onChangeCapture={innerInputChanged}>
+    <fieldset
+      className={css({ display: "flex", flexDirection: "column", gap: "8px" })}
+      ref={wrapperRef}
+      onChangeCapture={innerInputChanged}
+      disabled={pending}
+    >
       {children}
-    </Stack>
+    </fieldset>
   );
+}
+
+function isInputTarget(target: EventTarget): target is HTMLInputElement {
+  return "value" in target && "checked" in target;
 }
