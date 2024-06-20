fa8a6f8d5ca350ddf60e622f5ba71786e3f7046c
kiruushaaa <kiruxa4866613@gmail.com>
2024-06-20 18:02:50 +0300

    feat: reworked `Input` styling

--- panda.config.ts
+++ panda.config.ts
@@ -6,7 +6,7 @@ import { avatarRecipe } from "./src/components/uikit/Avatar/Avatar.recipe";
 import { buttonRecipe } from "./src/components/uikit/Button/Button.recipe";
 import { checkboxSlot } from "./src/components/uikit/Checkbox/Checkbox.recipe";
 import { dropdownSlot } from "./src/components/uikit/Dropdown/Dropdown.recipe";
-import { inputRecipe } from "./src/components/uikit/Input/Input.recipe";
+import { inputSlot } from "./src/components/uikit/Input/Input.recipe";
 import { premiseCardSlot } from "./src/components/uikit/PremiseCard/PremiseCard.recipe";
 import { radioSlot } from "./src/components/uikit/Radio/Radio.recipe";
 import { selectSlot } from "./src/components/uikit/Select/Select.recipe";
@@ -41,7 +41,6 @@ export default defineConfig({
       textStyles: additionalTextStyles,
       recipes: {
         button: buttonRecipe,
-        input: inputRecipe,
         avatar: avatarRecipe,
         icon: defineRecipe({
           className: "icon",
@@ -57,6 +56,7 @@ export default defineConfig({
         }),
       },
       slotRecipes: {
+        input: inputSlot,
         dropdown: dropdownSlot,
         select: selectSlot,
         footer: footerSlot,
--- src/components/uikit/Input/Input.tsx
+++ src/components/uikit/Input/Input.tsx
@@ -1,11 +1,52 @@
-import { ComponentPropsWithoutRef, ForwardedRef, forwardRef } from "react";
+import {
+  type ComponentPropsWithoutRef,
+  type ForwardedRef,
+  forwardRef,
+} from "react";
+import { cx } from "~/styled-system/css";
 import { input, type InputVariantProps } from "~/styled-system/recipes";
 
-export type InputProps = InputVariantProps & ComponentPropsWithoutRef<"input">;
+export type InputProps = InputVariantProps &
+  Omit<ComponentPropsWithoutRef<"input">, "prefix"> & {
+    prefix?: React.ReactNode;
+    suffix?: React.ReactNode;
+    "data-invalid"?: unknown;
+  };
 
 export const Input = forwardRef(function Input(
-  { variant, ...restProps }: InputProps,
+  {
+    variant,
+    rounded,
+    textCentered,
+    className,
+    prefix,
+    suffix,
+    ...restProps
+  }: InputProps,
   ref: ForwardedRef<HTMLInputElement>,
 ) {
-  return <input ref={ref} className={input({ variant })} {...restProps} />;
+  const classes = input({ variant, rounded, textCentered });
+  const inputClassName = cx(className, classes.label);
+  const {
+    hidden,
+    disabled,
+    "aria-invalid": ariaInvalid,
+    "data-invalid": dataInvalid,
+  } = restProps;
+  const invalidTag = [ariaInvalid, dataInvalid].some(Boolean) && {
+    "data-invalid": true,
+  };
+
+  return (
+    <label
+      className={inputClassName}
+      data-disabled={disabled}
+      hidden={hidden}
+      {...invalidTag}
+    >
+      {prefix}
+      <input ref={ref} className={classes.input} {...restProps} />
+      {suffix}
+    </label>
+  );
 });
