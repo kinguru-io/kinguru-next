427b9f9e0351bb86b104a60e19dca969b91d01fe
Maksim Yersh <edelwud@icloud.com>
2024-06-25 16:59:54 +0300

    chore: update stripe policy

--- src/lib/actions/booking/premise-slots-intent.ts
+++ src/lib/actions/booking/premise-slots-intent.ts
@@ -28,7 +28,7 @@ type BookTimeSlotsActionData = {
 };
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2024-04-10",
+  apiVersion: "2024-06-20",
 });
 
 export async function createPremiseSlotsIntent({
