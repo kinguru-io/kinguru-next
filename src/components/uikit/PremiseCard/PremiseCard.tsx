3b668cc8984196e5c150a8ad1a61385e27f48f10
kiruushaaa <kiruxa4866613@gmail.com>
2024-06-29 15:30:50 +0300

    Revert "chore: replaced `preserve-breaks` due to browser support"
    This reverts commit 3df9489b73ef4174654ea3982fd85c0dbe246fe6.

--- src/components/uikit/PremiseCard/PremiseCard.tsx
+++ src/components/uikit/PremiseCard/PremiseCard.tsx
@@ -41,7 +41,7 @@ export function PremiseDescription({ children }: PropsWithChildren) {
         color: "secondary",
         fontSize: "sm",
         lineHeight: "1.5",
-        whiteSpace: "pre",
+        whiteSpace: "preserve-breaks",
       })}
     >
       {children}
