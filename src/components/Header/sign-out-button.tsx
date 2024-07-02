b8b835ae37f3e804b753fda2d81f119f45812064
kiruushaaa <kiruxa4866613@gmail.com>
2024-07-02 12:25:28 +0300

    fix: added default user page instead of 404

--- src/components/Header/sign-out-button.tsx
+++ src/components/Header/sign-out-button.tsx
@@ -3,12 +3,14 @@
 import { signOut } from "next-auth/react";
 import type { ComponentPropsWithoutRef } from "react";
 
+const signOutClicked = () => signOut();
+
 export function SignOutButton({
   children,
   ...props
 }: ComponentPropsWithoutRef<"button">) {
   return (
-    <button {...props} onClick={() => signOut()}>
+    <button {...props} onClick={signOutClicked}>
       {children}
     </button>
   );
