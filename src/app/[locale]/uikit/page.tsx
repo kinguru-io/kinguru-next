cab992bd45f5d99f060b416342db1cfa02a8d707
kiruushaaa <kiruxa4866613@gmail.com>
2024-07-02 15:31:18 +0300

    chore: cleared up sample page

--- src/app/[locale]/uikit/page.tsx
+++ src/app/[locale]/uikit/page.tsx
@@ -1,44 +1,5 @@
-import { getTranslations } from "next-intl/server";
-import { EditUserProfileForm } from "./form";
-import { getSession } from "@/auth";
-import { ProfileSectionLayout } from "@/layout/page";
-import { updateUserProfile } from "@/lib/actions";
-import { redirect } from "@/navigation";
-import prisma from "@/server/prisma";
 import { Container } from "~/styled-system/jsx";
 
-export default async function Uikit() {
-  const session = await getSession();
-
-  if (!session || session.user?.role !== "user") {
-    return (
-      <Container paddingBlock="5rem">
-        The showcase only for a user role. Log in as a user.
-      </Container>
-    );
-  }
-  const user = await prisma.user.findUnique({
-    where: { id: session.user?.id || "" },
-    include: { socialLinks: { select: { network: true, url: true } } },
-  });
-
-  if (!user) {
-    return redirect("/auth/signin");
-  }
-
-  const t = await getTranslations("user.basic_info_page");
-
-  return (
-    <Container paddingBlock="5rem">
-      <ProfileSectionLayout>
-        <h1 className="heading">{t("heading")}</h1>
-        <section>
-          <EditUserProfileForm
-            userData={user}
-            updateProfile={updateUserProfile}
-          />
-        </section>
-      </ProfileSectionLayout>
-    </Container>
-  );
+export default function Page() {
+  return <Container paddingBlock="10">Sample page</Container>;
 }
