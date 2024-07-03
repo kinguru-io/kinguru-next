e31b8a6761bae6d0acb5307c4f8c55284c664092
kiruushaaa <kiruxa4866613@gmail.com>
2024-07-03 13:21:48 +0300

    chore: added icons

--- src/components/uikit/icon/sprite.gen.ts
+++ src/components/uikit/icon/sprite.gen.ts
@@ -1,5 +1,12 @@
 export interface SpritesMap {
-  action: "arrow" | "cross" | "menu" | "view-off" | "view";
+  action:
+    | "arrow"
+    | "cross"
+    | "image-add"
+    | "menu"
+    | "trash-can"
+    | "view-off"
+    | "view";
   common:
     | "calendar"
     | "location-star"
@@ -25,7 +32,7 @@ export const SPRITES_META: {
   };
 } = {
   action: {
-    filePath: "action.945d6b12.svg",
+    filePath: "action.0c90b4da.svg",
     items: {
       arrow: {
         viewBox: "0 0 20 20",
@@ -37,7 +44,17 @@ export const SPRITES_META: {
         width: 14,
         height: 14,
       },
+      "image-add": {
+        viewBox: "0 0 32 32",
+        width: 32,
+        height: 32,
+      },
       menu: {
+        viewBox: "0 0 24 24",
+        width: 24,
+        height: 24,
+      },
+      "trash-can": {
         viewBox: "0 0 24 24",
         width: 24,
         height: 24,
