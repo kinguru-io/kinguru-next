3152e941eac916350a10efd4fa60a6de95b3b7f3
kiruushaaa <kiruxa4866613@gmail.com>
2024-07-05 19:51:31 +0300

    chore: updated layer styles

--- src/theme/layerStyles.ts
+++ src/theme/layerStyles.ts
@@ -1,37 +1,14 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { defineLayerStyles } from "@pandacss/dev";
-import { LayerStyle } from "~/styled-system/types/composition";
-
-const outlineWrapperBaseStyles: LayerStyle = {
-  borderWidth: "2px",
-  borderStyle: "solid",
-  borderRadius: "10px",
-  backgroundColor: "light",
-};
 
 export const layerStyles = defineLayerStyles({
-  outlinePrimaryWrapper: {
+  "sub-section": {
     value: {
-      ...outlineWrapperBaseStyles,
-      borderColor: "primary",
-      paddingInline: "38px",
-      paddingBlock: "35px",
-    },
-  },
-  outlineSecondaryWrapper: {
-    value: {
-      ...outlineWrapperBaseStyles,
-      borderColor: "tertiary",
-      paddingBlockStart: "50px",
-      paddingBlockEnd: "60px",
-    },
-  },
-  dashedWrapper: {
-    value: {
-      borderWidth: "2px",
-      borderStyle: "dashed",
-      borderColor: "tertiary",
-      borderRadius: "10px",
+      paddingBlock: "3",
+      md: {
+        backgroundColor: "light",
+        padding: "8",
+      },
     },
   },
 });
