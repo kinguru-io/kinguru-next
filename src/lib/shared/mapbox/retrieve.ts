8316fc521d14a0eba595e488f8b3bf360b137403
Maksim Yersh <edelwud@icloud.com>
2024-07-12 17:49:13 +0300

    feat: remove logger and intermediate variable

--- src/lib/shared/mapbox/retrieve.ts
+++ src/lib/shared/mapbox/retrieve.ts
@@ -1,8 +1,6 @@
 import { type SearchBoxRetrieveResponse } from "@mapbox/search-js-core";
-import { getLogger } from "@/lib/logger.ts";
 
 export async function retrieveLocationPropertiesById(mapboxId: string) {
-  const logger = getLogger();
   const retrieveUrl = new URL(
     `https://api.mapbox.com/search/searchbox/v1/retrieve/${mapboxId}`,
   );
@@ -16,9 +14,9 @@ export async function retrieveLocationPropertiesById(mapboxId: string) {
     next: { revalidate: 3600 },
   });
 
-  const retrieveResponse: SearchBoxRetrieveResponse = await response.json();
+  const {
+    features: [{ properties }],
+  }: SearchBoxRetrieveResponse = await response.json();
 
-  logger.error({ ...retrieveResponse, mapbox_token: process.env.MAPBOX_TOKEN });
-
-  return retrieveResponse.features.at(0)?.properties;
+  return properties;
 }
