7e09e35a6f4312df65faa32848fdf12241d96c2b
kiruushaaa <kiruxa4866613@gmail.com>
2024-07-23 18:43:03 +0300

    chore: restyled email verification page

--- src/app/[locale]/verify/page.tsx
+++ src/app/[locale]/verify/page.tsx
@@ -1,10 +1,11 @@
 import { useTranslations } from "next-intl";
 import { getTranslations } from "next-intl/server";
 import { Suspense } from "react";
-import { verifyEmail } from "@/app/[locale]/verify/verify-email";
-import { SpinnerIcon } from "@/components/uikit";
+import { verifyEmail } from "./verify-email";
+import { Icon, SpinnerIcon } from "@/components/uikit";
 import { Link, redirect } from "@/navigation";
-import { Center } from "~/styled-system/jsx";
+import { css } from "~/styled-system/css";
+import { Center, VStack } from "~/styled-system/jsx";
 import { button } from "~/styled-system/recipes";
 
 export default function VerifyEmailPage({
@@ -17,7 +18,7 @@ export default function VerifyEmailPage({
   if (!token) return redirect("/");
 
   return (
-    <Center css={{ height: "full", flexDirection: "column", gap: "4" }}>
+    <Center css={{ height: "full" }}>
       <Suspense
         fallback={
           <>
@@ -34,22 +35,40 @@ export default function VerifyEmailPage({
 
 async function EmailCheck({ token }: { token: string }) {
   const t = await getTranslations("verification");
-  const { message } = await verifyEmail(token);
+  const { message, ok } = await verifyEmail(token);
 
   // TODO add email input to resend?
 
   return (
-    <>
-      <span>{message}</span>
-      <Link className={button({ colorPalette: "primary" })} href="/">
+    <VStack gap="4" paddingBlock="6">
+      <VStack gap="4" textAlign="center" maxWidth="md" fontSize="sm">
+        <Icon
+          className={css({
+            padding: "3",
+            borderRadius: "full",
+            colorPalette: ok ? "success" : "danger",
+            color: "colorPalette.text",
+            bgColor: "colorPalette",
+            fontWeight: "bold",
+            fontSize: "3xl",
+          })}
+          name={ok ? "action/tick-double" : "action/cross"}
+        />
+        {message}
+      </VStack>
+      <Link className={button()} href="/">
         {t("main_page_link")}
       </Link>
       <Link
-        className={button({ colorPalette: "secondary" })}
+        className={css({
+          fontSize: "sm",
+          color: "secondary",
+          _hoverOrFocusVisible: { textDecoration: "underline" },
+        })}
         href="/profile/edit"
       >
         {t("profile_page_link")}
       </Link>
-    </>
+    </VStack>
   );
 }
