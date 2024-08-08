195e5a55ed25ab8281b304c4a727acc3bd131f8a
kiruushaaa <kiruxa4866613@gmail.com>
2024-08-08 12:15:31 +0300

    chore: removed russian for calendar

--- src/lib/shared/config/date-fns-locale-map.ts
+++ src/lib/shared/config/date-fns-locale-map.ts
@@ -1,8 +1,7 @@
-import { ru, pl, enUS, type Locale as DateFnsLocale } from "date-fns/locale";
+import { pl, enUS, type Locale as DateFnsLocale } from "date-fns/locale";
 import type { Locale } from "@/navigation";
 
 export const localeMap: Record<Locale, DateFnsLocale> = {
   en: enUS,
-  ru: ru,
   pl: pl,
 };
