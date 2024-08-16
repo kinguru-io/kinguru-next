685c8d71f7d0d541f0a7a21a2349535afa754c19
kiruushaaa <kiruxa4866613@gmail.com>
2024-08-16 13:30:14 +0300

    feat: added YM

---
+++ src/components/analytics/ym-scripts.tsx
@@ -0,0 +1,51 @@
+import Image from "next/image";
+import Script from "next/script";
+
+export const counterYM = 98083142;
+
+export function YandexAnalytics() {
+  return (
+    <>
+      <Script id="yandex-analytics">
+        {`
+          (function (m, e, t, r, i, k, a) {
+            m[i] =
+              m[i] ||
+              function () {
+                (m[i].a = m[i].a || []).push(arguments);
+              };
+            m[i].l = 1 * new Date();
+            for (var j = 0; j < document.scripts.length; j++) {
+              if (document.scripts[j].src === r) {
+                return;
+              }
+            }
+            (k = e.createElement(t)),
+              (a = e.getElementsByTagName(t)[0]),
+              (k.async = 1),
+              (k.src = r),
+              a.parentNode.insertBefore(k, a);
+          })(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+          ym(${counterYM}, "init", {
+            defer:true,
+            clickmap:true,
+            trackLinks:true,
+            accurateTrackBounce:true,
+            webvisor:true
+          });
+        `}
+      </Script>
+      <noscript>
+        <div>
+          <Image
+            src={`https://mc.yandex.ru/watch/${counterYM}`}
+            style={{ position: "absolute", left: "-9999px" }}
+            fetchPriority="low"
+            alt=""
+          />
+        </div>
+      </noscript>
+    </>
+  );
+}
--- src/components/google-analytics/index.ts
+++
@@ -1,2 +0,0 @@
-export { GoogleAnalyticsConsent } from "./ga-consent";
-export { GoogleAnalytics } from "./ga-scripts";
