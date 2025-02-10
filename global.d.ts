1a4158ed677eb575230fbd8f1290e842dcefbccb
Alinur Dzjeenbekov <alinur.ali.07@mail.ru>
2025-02-10 22:41:43 +0600

    fix: type of window build error

--- global.d.ts
+++ global.d.ts
@@ -1,20 +1,32 @@
 import { PrismaClient } from "@prisma/client";
 
-type Messages = typeof import("./public/locales/en/common.json")
+type Messages = typeof import("./public/locales/en/common.json");
 
 declare global {
   namespace globalThis {
     var prisma: PrismaClient;
   }
+
   namespace NodeJS {
     interface Global {
       prisma: PrismaClient;
     }
   }
+
   interface GtagWindow extends Window {
     gtag: any;
   }
+
   interface IntlMessages extends Messages {}
+
+  interface Window extends GtagWindow {
+    googleTranslateElementInit: () => void;
+    google?: {
+      translate?: {
+        TranslateElement: new (options: object, containerId: string) => void;
+      };
+    };
+  }
 }
 
-declare var window: GtagWindow & typeof globalThis;
+export {};
